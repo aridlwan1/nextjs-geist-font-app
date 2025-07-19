534337f3b48cacc849dedf87d58d809d91de5e12
Ahmad Ridlwan <36151363+aridlwan1@users.noreply.github.com>
2025-07-19 23:09:42 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752941382653

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_227bc5d6._.js",
+  "static/chunks/src_b9c5611b._.js"
+],
+    source: "dynamic"
+});
